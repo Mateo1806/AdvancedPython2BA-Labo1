c5c0a172bd0b1c0e903bbfffea9f4003ee707c98
Mateo1806 <mathepirat1806@gmail.com>
2020-02-13 10:58:53 +0100

    first commit

--- utils.py
+++ utils.py
@@ -2,24 +2,22 @@
 # Math library
 # Author: Sébastien Combéfis
 # Version: February 8, 2018
+from math import sqrt
 
 def fact(n):
-	"""Computes the factorial of a natural number.
-	
-	Pre: -
-	Post: Returns the factorial of 'n'.
-	Throws: ValueError if n < 0
-	"""
-	pass
+	x = 1
+	for i in range (0,n):
+		x = x*(i+1)
+	return x
 
 def roots(a, b, c):
-	"""Computes the roots of the ax^2 + bx + x = 0 polynomial.
-	
-	Pre: -
-	Post: Returns a tuple with zero, one or two elements corresponding
-		to the roots of the ax^2 + bx + c polynomial.
-	"""
-	pass
+	d = b**2 - 4*a*c
+	if d < 0 :
+		return ()
+	if d == 0:
+		return ((-b )/(2*a),) #virgule après la parenthèse sinon il considère comme un nombre et pas comme un tuple
+	if d > 0 :
+		return ((-b + sqrt(d))/(2*a), (-b - sqrt(d))/(2*a))
 
 def integrate(function, lower, upper):
 	"""Approximates the integral of a fonction between two bounds
@@ -34,7 +32,18 @@ def integrate(function, lower, upper):
 		you'll probably need the 'eval' function to evaluate the function
 		to integrate given as a string.
 	"""
-	pass
+	x = 1
+	try:
+		_ = eval (function) #_ c'est une variable qui n'est pas utilisée
+	except:
+		"error in fuction"
+	if upper < lower:
+		return "upper is smaller than lower bound"
+	from scipy import integrate
+	fct = lambda x: eval(function)
+	result = integrate.quad(fct, lower, upper)
+	return result[0]
+	
 
 if __name__ == '__main__':
 	print(fact(5))
